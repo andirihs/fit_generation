fbf3a05ce7fceedaad816ca29f65d30466f1f8cf
andy <andi.rihs@bluewin.ch>
2022-05-08 21:20:48 +0200

    Revert "feat: bottomNavBar swipable using PageView"
    This reverts commit 4cc1ff43

--- lib/src/shared_scaffold.dart
+++ lib/src/shared_scaffold.dart
@@ -1,7 +1,4 @@
-import 'package:fit_generation/src/chat_feat/chat_view.dart';
 import 'package:fit_generation/src/routing/app_router.dart';
-import 'package:fit_generation/src/sample_feat/sample_item_list_view.dart';
-import 'package:fit_generation/src/wight_tracker_feat/weight_tracker_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -27,8 +24,6 @@ class SharedScaffold extends StatefulWidget {
 class _SharedScaffoldState extends State<SharedScaffold> {
   int _selectedIndex = 0;
 
-  final _pageController = PageController();
-
   @override
   void didUpdateWidget(SharedScaffold oldWidget) {
     super.didUpdateWidget(oldWidget);
@@ -38,36 +33,13 @@ class _SharedScaffoldState extends State<SharedScaffold> {
   /// Rebuild the BottomNavBar and navigate to the corresponding view/route.
   void tap(BuildContext context, int index) {
     setState(() => _selectedIndex = index);
-    _pageController.jumpToPage(index);
     context.goNamed(AppRouter.getNamedLocationFromIndex(index));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: widget.body
-      /// https://pub.dev/packages/bottom_navy_bar
-      body: SizedBox.expand(
-        child: PageView(
-          onPageChanged: (index) => setState(() => _selectedIndex = index),
-
-          /// must match the order of the button nav items and router methods.
-          children: const [
-            SampleItemListView(),
-            ChatView(),
-            WeightTrackerView(),
-          ],
-          controller: _pageController,
-        ),
-      ),
-
-      /// https://pub.dev/packages/bubble_bottom_bar
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.red,
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      body: widget.body,
 
       /// selected index == -1 => navigation outside of the
       /// bottomNavigationBar stack -> buttonNavigation not visible.
